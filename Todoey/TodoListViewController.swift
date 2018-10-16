39935ba1bf4b6c58b60393629a6c8eb469c402f7
codyByCherry <373466869@qq.com>
2018-10-16 16:33:18 +0800

    show three items in the tableview.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    let todoArray:[String] = ["Find Milk", "Get Egg", "Write Log"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+    
+    //MARK- TableView DataSource
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell")
+        cell?.textLabel?.text = todoArray[indexPath.row]
+        return cell!
     }
 
 
