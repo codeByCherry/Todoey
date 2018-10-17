86d27914acb8625d7a6b3f8b0fda5d509fc52c98
codyByCherry <373466869@qq.com>
2018-10-17 17:57:43 +0800

    `add new View, 显示Category

---
+++ Todoey/Control/CategoryViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Tony Zhang on 2018/10/17.
+//  Copyright © 2018年 SSM. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+}
