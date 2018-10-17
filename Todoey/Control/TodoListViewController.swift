3bd1703a1d9aca2306de00d6b24643db1fad3811
codyByCherry <373466869@qq.com>
2018-10-17 14:27:32 +0800

    coredata 删除操作

--- Todoey/Control/TodoListViewController.swift
+++ Todoey/Control/TodoListViewController.swift
@@ -62,7 +62,24 @@ class TodoListViewController: UITableViewController {
         
     }
     
-
+    override func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+        return "Del"
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            let curItem = itemArr[indexPath.row]
+            itemArr.remove(at: indexPath.row)
+            context.delete(curItem)
+            saveItems()
+            tableView.deleteRows(at: [indexPath], with: .left)
+        }
+        
+    }
+    
+    
     // MARK:- Add new item.
     @IBAction func addItemPressed(_ sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "Add New Todoey item", message: "", preferredStyle: .alert)
@@ -123,10 +140,12 @@ class TodoListViewController: UITableViewController {
         // 推荐下面的写法，更加OOP
         //let fr = NSFetchRequest<Item>(entityName:"Item")
         let fr2:NSFetchRequest<Item> = Item.fetchRequest()
+
         context.performAndWait {
             
             do {
                 self.itemArr = try context.fetch(fr2)
+                
             } catch {
                 print("load items error:\(error)")
             }
